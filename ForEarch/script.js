b18ace1b99d867126290e1416f0f1909a47b49a2
Noxus2007 <kevinandresreatiga@gmail.com>
2025-08-12 20:01:49 -0500

    funcionEarch

--- ForEarch/script.js
+++ ForEarch/script.js
@@ -1,21 +1,28 @@
+const earch = document.getElementById ("")
 
 const lista = [
-    "Kiwi", "Ventilador", "Juan", "Lechuga", "Pechuga", "Danna"
+ "Danna", "Ari " , "bryan " , "Lucas " , "Nicol "
 ]
 
-const contenedor = document .getElementById("Area");
-function actualizarHTML(){
+const Area = document.getElementById("Area");
+function actualizarHTML (){
     let resultHTML = "";
-    lista.forEach((Area, index) => {
-        resultHTML +=`<span id="forEach${index}">${Area}</span>`
-    }) 
-    contenedor.innerHTML = resultHTML;
+    lista.forEach((elemento,index) => {
+        resultHTML += `<span id="forEach${index}">${elemento}</span>`
+    })
+    Area.innerHTML = resultHTML;
 }
 
 actualizarHTML()
 
-lista.forEach((value,index)=>{
-    console.log("valor"+ value)
-    console.log("Indice + index")
-    console.log("")
-})
+function mifuncionforEach(){
+    lista.forEach((value,index)=> {
+        const elemento = document.getElementById(`forEach${index}`)
+        setTimeout(()=>{
+            elemento.classList.add("selected")
+        }, 2000 * index)
+     setTimeout(()=> {
+        elemento.classList.remove("selected")
+     }, 2000 * (index + 1))
+    })
+}
